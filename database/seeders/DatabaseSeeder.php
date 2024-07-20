7ca6f4e9b965295da3b8efe5b1556492f567fdbc
manirdev <mhtnourmhtmjr@gmail.com>
2024-07-21 00:31:35 +0300

    DB seeder cooment is opened

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            //CategorySeeder::class,
-            //ProductSeeder::class,
+            CategorySeeder::class,
+            ProductSeeder::class,
             ImageSeeder::class,
         ]);
     }
