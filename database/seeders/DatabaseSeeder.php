8933e62a0896afb1566a50be586f3ac0c0edd2f3
manirdev <mhtnourmhtmjr@gmail.com>
2024-07-21 18:31:57 +0300

    files are modified

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             CategorySeeder::class,
-            ProductSeeder::class,
-            ImageSeeder::class,
+            //ProductSeeder::class,
+            //ImageSeeder::class,
         ]);
     }
 }
