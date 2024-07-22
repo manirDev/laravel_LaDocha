e2c51790156fcf8c22bedb6dd8f75ca79a22c5a3
manirdev <mhtnourmhtmjr@gmail.com>
2024-07-22 08:38:15 +0300

    banners modification and pages are also updated

--- resources/views/frontend/pages/faqPage.blade.php
+++ resources/views/frontend/pages/faqPage.blade.php
@@ -17,7 +17,7 @@
                         <span class="title-tag">Last Updated on November 02, 2014</span>
                         <div class="panel-group checkout-steps" id="accordion">
                             @foreach($faqs as $item)
-                                <div class="panel panel-default checkout-step-01">
+                                <div class="panel panel-default checkout-step-{{$item->position}}">
 
                                     <!-- panel-heading -->
                                     <div class="panel-heading">
@@ -45,73 +45,7 @@
                 </div><!-- /.row -->
             </div><!-- /.checkout-box -->
             <!-- ============================================== BRANDS CAROUSEL ============================================== -->
-            <div id="brands-carousel" class="logo-slider wow fadeInUp">
-
-                <div class="logo-slider-inner">
-                    <div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
-                        <div class="item m-t-15">
-                            <a href="#" class="image">
-                                <img data-echo="assets/images/brands/brand1.png" src="assets/images/blank.gif" alt="">
-                            </a>
-                        </div><!--/.item-->
-
-                        <div class="item m-t-10">
-                            <a href="#" class="image">
-                                <img data-echo="assets/images/brands/brand2.png" src="assets/images/blank.gif" alt="">
-                            </a>
-                        </div><!--/.item-->
-
-                        <div class="item">
-                            <a href="#" class="image">
-                                <img data-echo="assets/images/brands/brand3.png" src="assets/images/blank.gif" alt="">
-                            </a>
-                        </div><!--/.item-->
-
-                        <div class="item">
-                            <a href="#" class="image">
-                                <img data-echo="assets/images/brands/brand4.png" src="assets/images/blank.gif" alt="">
-                            </a>
-                        </div><!--/.item-->
-
-                        <div class="item">
-                            <a href="#" class="image">
-                                <img data-echo="assets/images/brands/brand5.png" src="assets/images/blank.gif" alt="">
-                            </a>
-                        </div><!--/.item-->
-
-                        <div class="item">
-                            <a href="#" class="image">
-                                <img data-echo="assets/images/brands/brand6.png" src="assets/images/blank.gif" alt="">
-                            </a>
-                        </div><!--/.item-->
-
-                        <div class="item">
-                            <a href="#" class="image">
-                                <img data-echo="assets/images/brands/brand2.png" src="assets/images/blank.gif" alt="">
-                            </a>
-                        </div><!--/.item-->
-
-                        <div class="item">
-                            <a href="#" class="image">
-                                <img data-echo="assets/images/brands/brand4.png" src="assets/images/blank.gif" alt="">
-                            </a>
-                        </div><!--/.item-->
-
-                        <div class="item">
-                            <a href="#" class="image">
-                                <img data-echo="assets/images/brands/brand1.png" src="assets/images/blank.gif" alt="">
-                            </a>
-                        </div><!--/.item-->
-
-                        <div class="item">
-                            <a href="#" class="image">
-                                <img data-echo="assets/images/brands/brand5.png" src="assets/images/blank.gif" alt="">
-                            </a>
-                        </div><!--/.item-->
-                    </div><!-- /.owl-carousel #logo-slider -->
-                </div><!-- /.logo-slider-inner -->
-
-            </div><!-- /.logo-slider -->
+            @include('frontend.body.brands')
             <!-- ============================================== BRANDS CAROUSEL : END ============================================== -->	</div><!-- /.container -->
     </div><!-- /.body-content -->
 @endsection
